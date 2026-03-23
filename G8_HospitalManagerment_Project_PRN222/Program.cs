c37f20b62c21533b476d2b1397f77d1ab807e240
hieudnhe181465 <hieudnhe181465@fpt.edu.vn>
2026-03-23 09:03:31 +0700

    feature: login, register, reset password, changepassword, viewprofile, updateprofile

--- G8_HospitalManagerment_Project_PRN222/Program.cs
+++ G8_HospitalManagerment_Project_PRN222/Program.cs
@@ -8,6 +8,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DbHospitalManagementContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("MyCnn")));
+
+builder.Services.AddSession();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,7 +21,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
